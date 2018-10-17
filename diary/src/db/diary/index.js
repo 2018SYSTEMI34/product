5ecf17566f9ef4f766e69b74a811383029f38899
test <test@xxx.co.jp>
2018-10-17 12:32:01 +0900

    Update関数を追加

--- diary/src/db/diary/index.js
+++ diary/src/db/diary/index.js
@@ -7,6 +7,7 @@ import NeDB from 'nedb';
  * DiaryDB初期化
  * データ構造
  * {
+ *  _id:string,
  *  write_on:string,
  *  day:string,
  *  sentence:string,
@@ -24,10 +25,18 @@ db.diary = new NeDB({
  * jsonデータを受け取り、
  * Diaryテーブルデータを登録する。
  * 
- * @param {登録データ} obj 
- * @returns bool
+ * 受け取るjsonの形式
+ * {
+ *  write_on:string,
+ *  day:string,
+ *  sentence:string,
+ *  kind:array[string]
+ * }
+ * 
+ * @param {json} obj 
+ * @returns boolean
  */
-const insert = (obj) => {
+export const insert = (obj) => {
 
     if(!obj) {
         console.log("obj is illegal")
@@ -49,12 +58,92 @@ const insert = (obj) => {
         return false;
     };
 
+    // データの入れ替え
+    const date = {
+        write_on : obj.write_on,
+        day      : obj.day,
+        sentence : obj.sentence,
+        kind     : obj.kind
+    }
+
+    // DB読み込み
+    db.diary.loadDatabase();
+
     // DB登録
-    db.diary.insert(obj,function(err,newDoc) {
+    db.diary.insert(data,function(err,newDoc) {
         console.log("[INSERT]")
         console.log(newDoc)
         return false;
     });
 
     return true;
+}
+
+/**
+ * Dairyテーブルへの更新
+ * 
+ * jsonデータを受け取り、
+ * テーブルレコードを更新する。
+ * 
+ * 受け取るjsonの形式
+ * {
+ *  _id : string,
+ *  write_on:string,
+ *  day:string,
+ *  sentence:string,
+ *  kind:array[string]
+ * }
+ * 
+ * @param {json} obj
+ * @returns boolean
+ */
+export const update = (obj) => {
+
+    if(!obj) {
+        console.log("obj is illegal")
+        return false;
+    };
+
+    if(!obj._id) {
+        console.log("_id is illegal")
+        return false;
+    }
+
+    if(!obj.write_on) {
+        console.log("write_on is illegal")
+        return false;
+    };
+
+    if(!obj.day) {
+        console.log("day is illegal")
+        return false;
+    };
+
+    if(!obj.sentence) {
+        console.log("sentence is illegal")
+        return false;
+    };
+
+    // DB読み込み
+    db.diary.loadDatabase();
+
+    // DB更新
+    db.diary.upadate(
+        {_id : obj._id},
+        {
+          write_on : obj.write_on,
+          day:obj.day,
+          sentence:obj.sentence,
+          kind:obj.kind
+        },
+        {},
+        function(err,numReplaced) {
+            console.log("[UPDATE]");
+            console.log(numReplaced);
+            return false;
+        }
+    )
+
+    return true;
+
 }
