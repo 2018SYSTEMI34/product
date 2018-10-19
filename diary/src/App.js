80ee10950a15564b9a25ec00a000cb52ddb60d69
mochizuki-systemi <hiromi.mochizuki@systemi.co.jp>
2018-10-19 18:13:08 +0900

    viewsフォルダ作成

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './Header.js'
-import Footer from './Footer.js'
+import Header from './views/Header.js'
+import Footer from './views/Footer.js'
 
 class App extends Component {
   render() {
--- diary/src/views/Footer.js
+++ diary/src/views/Footer.js
@@ -3,7 +3,7 @@ import home from './image/home.svg'
 import search from './image/search.svg'
 import registration from './image/registration.svg'
 import backup from './image/backup.svg'
-import './App.css';
+import '../App.css';
 
 //フッター
 class Footer extends Component{
@@ -12,19 +12,19 @@ class Footer extends Component{
       <footer className="App-footer">
         <ul className="footer-ul"> 
           <li className="footer-menu">
-            <li className="footer-menu-li"><img src={home} className="App-image"/></li>
+            <li className="footer-menu-li"><img src={home} className="App-image" alt="home"/></li>
             <li className="footer-menu-li">ホーム</li>
           </li>
           <li className="footer-menu">
-            <li className="footer-menu-li"><img src={search} className="App-image"/></li>
+            <li className="footer-menu-li"><img src={search} className="App-image" alt="search"/></li>
             <li className="footer-menu-li">検索</li>
           </li>
           <li className="footer-menu">
-            <li className="footer-menu-li"><img src={registration} className="App-image"/></li>
+            <li className="footer-menu-li"><img src={registration} className="App-image" alt="registration"/></li>
             <li className="footer-menu-li">日記を書く</li>
           </li> 
           <li className="footer-menu">
-            <li className="footer-menu-li"><img src={backup} className="App-image"/></li>
+            <li className="footer-menu-li"><img src={backup} className="App-image" alt="backup"/></li>
             <li className="footer-menu-li">バックアップ</li>
           </li>
         </ul>
