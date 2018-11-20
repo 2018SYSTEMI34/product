5dc526aa71a641adca5b11ac55931a1248e233bb
mochizuki-systemi <hiromi.mochizuki@systemi.co.jp>
2018-11-20 22:24:07 +0900

    一覧表示画面の修正

--- diary/src/views/Input.js
+++ diary/src/views/Input.js
@@ -9,9 +9,9 @@ const Input = () => {
           <input type="date" />
         </div>
         <div>
-          <textarea cols="50" classaname="input" rows="25" maxlength="" placeholder="日記を入力"></textarea>
+          <textarea classaname="input" cols="45" rows="25" maxlength="" placeholder="日記を入力"></textarea>
         </div>
-        <input type="button" className="button" value="保存"/>
+        <input type="button" className="button" value="  保  存 "/>
       </div>
     </div>
   );
