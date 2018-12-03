3f562a6f7e5bd8cafc5696079bc7c1ef4fb164ef
watanabe-systemi <y.watanabe@systemi.co.jp>
2018-12-04 07:32:05 +0900

    参照成功

--- dairy/src/views/Disp.js
+++ dairy/src/views/Disp.js
@@ -5,6 +5,7 @@
 import React, { Component } from 'react';
 import { View, Text ,FlatList,Image } from 'react-native';
 import Style from '../style';
+import Dairy from '../db/diary';
 
 /**
  * 一覧表示画面オブジェクト
@@ -13,17 +14,10 @@ export default class DispView extends Component {
 
     constructor(props) {
         super(props);
+        let dairy = new Dairy();
         //TODO Propsから受け取るようにする
         this.state = {
-            days : [
-                {year:"2018", day:"11/1", dow:"(月)",content:"HelloWorld"},
-                {year:"2018", day:"11/2", dow:"(火)",content:"HelloWorld"},
-                {year:"2018", day:"11/3", dow:"(水)",content:"HelloWorld"},
-                {year:"2018", day:"11/4", dow:"(木)",content:"HelloWorld"},
-                {year:"2018", day:"11/5", dow:"(金)",content:"HelloWorld"},
-                {year:"2018", day:"11/6", dow:"(土)",content:"HelloWorld"},
-                {year:"2018", day:"11/7", dow:"(日)",content:"HelloWorld"}
-            ],
+            records : dairy.allRecords(),
         }
     }
 
@@ -32,7 +26,7 @@ export default class DispView extends Component {
     render() {
         return (
             <View>
-                <FlatList data={this.state.days} renderItem={this.renderItem} keyExtractor={this.keyExtractor} />
+                <FlatList data={this.state.records} renderItem={this.renderItem} keyExtractor={this.keyExtractor} />
             </View>
         )
     }
@@ -42,12 +36,12 @@ export default class DispView extends Component {
         return (
             <View style={Style.SingleArea}>
                 <View style={Style.DispContent}>
-                    <Text style={Style.DispDateText}>{item.year}</Text>
-                    <Text style={Style.DispDateText}>{item.day}</Text>
-                    <Text style={Style.DispDateText}>{item.dow}</Text>
+                    <Text style={Style.DispDateText}>{item.writed_on.getFullYear()}</Text>
+                    <Text style={Style.DispDateText}>{item.writed_on.getMonth() + 1}/{item.writed_on.getDate()}</Text>
+                    <Text style={Style.DispDateText}>{this.changeDay(item.writed_on.getDay())}</Text>
                 </View>
                 <View style={Style.DispContentMain}>
-                    <Text style={Style.DispDateText}>{item.content}</Text>
+                    <Text style={Style.DispDateText}>{item.sentence}</Text>
                 </View>
                 <View style={Style.DispContent}>
                     <Image source={require('../img/twitter.png')} style={Style.DispImage}/>
@@ -61,30 +55,9 @@ export default class DispView extends Component {
     keyExtractor = (item,index) => {
         return String(index);
     }
-}
 
-/**
-const styles = {
-  pageStyle: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 100
-  },
-  fontStyle: {
-    fontSize: 36,
-    fontWeight: '600'
-  }
+    //日本語の曜日を返す
+    changeDay = (day) => {
+        return [ "日", "月", "火", "水", "木", "金", "土" ][day]; 
+    }
 }
-
-class Disp extends Component {
-  render() {
-    return (
-      <View style={styles.pageStyle}>
-        <Text style={styles.fontStyle}>Article</Text>
-      </View>
-    )
-  }
-}
-
-export default Disp;
-*/
